fa7d87d1c74660e45cf30897a4f37c871a8b9b6e
German Medina <urugerman@gmail.com>
2024-07-22 21:07:27 -0300

    Ajustes a entrega 2

--- src/routes/views.route.js
+++ src/routes/views.route.js
@@ -8,13 +8,13 @@ const productManager = new ProductManager(__dirname + '/data/productos.json');
 router.get('/', async (req, res) => {
     await productManager.verificarFileJson();
     const products = await productManager.getProductList();
-    res.render('home', { products });
+    res.render('home', { products, titulo : "Home" });
 });
 
 router.get('/realtimeproducts', async (req, res) => {
     await productManager.verificarFileJson();
     const products = await productManager.getProductList();
-    res.render('realtimeProducts', { products });
+    res.render('realtimeProducts', { products, titulo : "RealTime Products" });
 });
 
 export default router;
